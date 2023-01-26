d5a202c4eabdb9db39e4a983234e9e22994bef50
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2023-01-26 13:10:30 +0530

    Delete tracert.py

--- tracert.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import csv
-ipORurl = input()
-with open("output.csv", "w") as file:
-    process = subprocess.Popen(["tracert", ipORurl], stdout=subprocess.PIPE)
-    for line in process.stdout:
-        line_list = line.decode('utf-8').split()
-        file.write(','.join(line_list) + '\n')
